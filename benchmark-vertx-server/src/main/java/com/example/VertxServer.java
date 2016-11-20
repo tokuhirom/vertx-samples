aa073dc14ae7e5caedc5fdefcee99e606bc66781
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-11-21 02:45:55 +0900

    bench server

---
+++ benchmark-vertx-server/src/main/java/com/example/VertxServer.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import io.vertx.rxjava.core.Vertx;
+import io.vertx.rxjava.core.http.HttpServer;
+import io.vertx.rxjava.ext.web.Router;
+import io.vertx.rxjava.ext.web.RoutingContext;
+
+public class VertxServer {
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        HttpServer httpServer = vertx.createHttpServer();
+        Router router = Router.router(vertx);
+        router.route("/").handler(c -> {
+            c.response().end("OK");
+        });
+        httpServer.requestHandler(router::accept)
+                .listen(8080);
+    }
+}
