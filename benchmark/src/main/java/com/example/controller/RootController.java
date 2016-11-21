563f6e2295f6d4414dc4dd34c722e7891ea1506e
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-11-21 13:08:23 +0900

    Update RootController.java

--- benchmark/src/main/java/com/example/controller/RootController.java
+++ benchmark/src/main/java/com/example/controller/RootController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class RootController {
     @GetMapping("/")
     public String proxy() {
-        new Thread().start();
         return "OK";
     }
 }
